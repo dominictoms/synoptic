f5fee285727d7fce62dbc5d0d425723e014cb22d
MPSTR <74561948+MPSTR24@users.noreply.github.com>
2021-06-14 17:30:25 +0100

    Changed profile page and made minor QoL tweaks

--- davis/www/js/message-board.js
+++ davis/www/js/message-board.js
@@ -1,20 +1,27 @@
 //location of name in header
-const contactName = document.getElementById("header-contact-name");
-//the blank "start messaging" panel
-const blankPanel = document.getElementById("blank-panel");
+var contactName = document.getElementById("header-contact-name"); //the blank "start messaging" panel
 
-//Giving the contact clickability
-const contact = document.getElementsByClassName("contact");
-for (let i = 0; i < contact.length; i++) {
+var blankPanel = document.getElementById("blank-panel"); //Giving the contact clickability
+
+var footer = document.getElementById("footer");
+
+var chat = document.getElementById("chat");
+
+var contact = document.getElementsByClassName("contact");
+
+var _loop = function _loop(i) {
    contact[i].addEventListener("click", function () {
       function1(i);
    });
-}
+};
 
-//Changing the message panel
+for (var i = 0; i < contact.length; i++) {
+   _loop(i);
+} //Changing the message panel
+
 function function1(contactID) {
-   var data = contact[contactID].innerText.split("\n");
-   //To detect if we are on a mobile device I will use some code found here (https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device)
+   var data = contact[contactID].innerText.split("\n"); //To detect if we are on a mobile device I will use some code found here (https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device)
+
    if (
       /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
          navigator.userAgent
@@ -24,38 +31,21 @@ function function1(contactID) {
       window.location.replace("messages.html?userId=" + data[0] + "");
    } else {
       //If the user is on a desktop we can use the split display
-
       //currently using the name to go the the message page
-
-      contactName.innerText = data[0];
-      //If the user has clicked on a contact we can remove this panel for this session
+      contactName.innerText = data[0]; //If the user has clicked on a contact we can remove this panel for this session
+      footer.style.display = "block";
+      chat.style.height = "calc(100% - 118px)";
       blankPanel.remove();
    }
 }
 
 function addContact(imageURL, contactName, lastMessage) {
    document.getElementById("contact-panel").innerHTML +=
-      `
-   <div class="row contact-row flex-nowrap">
-      <a href="profile.html">
-         <div class="col-3 contact-left">
-            <img src="` +
+      '\n   <div class="row contact-row flex-nowrap">\n      <a href="profile.html">\n         <div class="col-3 contact-left">\n            <img src="' +
       imageURL +
-      `" alt="" />
-         </div>
-      </a>
-      <a class="contact">
-         <div class="col-9 contact-right">
-            <div class="contact-name">` +
+      '" alt="" />\n         </div>\n      </a>\n      <a class="contact">\n         <div class="col-9 contact-right">\n            <div class="contact-name">' +
       contactName +
-      `</div>
-            <div class="contact-message">
-                  ` +
+      '</div>\n            <div class="contact-message">\n                  ' +
       lastMessage +
-      `
-            </div>
-         </div>
-      </a>
-   </div>
-   `;
+      "\n            </div>\n         </div>\n      </a>\n   </div>\n   ";
 }
