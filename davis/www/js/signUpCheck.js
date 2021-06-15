e979f487fd93e81351757ff9cd6fed4d5b0d62b2
MPSTR <74561948+MPSTR24@users.noreply.github.com>
2021-06-15 02:02:38 +0100

    Signup form frontend string length checks implemented

---
+++ davis/www/js/signUpCheck.js
@@ -0,0 +1,25 @@
+function validateForm() {
+   var signupform = document.forms.signupform;
+
+   var signupformData = new FormData(signupform);
+
+   var formEmail = signupformData.get("email");
+
+   var formForename = signupformData.get("forename");
+
+   var formSurname = signupformData.get("surname");
+
+   var formUsername = signupformData.get("username");
+
+   if (
+      (formEmail.length > 255) |
+      (formUsername.length > 20) |
+      (formSurname.length > 20) |
+      (formForename.length > 20)
+   ) {
+      console.log("invalid");
+      return false;
+   } else {
+      return true;
+   }
+}
